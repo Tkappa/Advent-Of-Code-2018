b7583e0a3dc02b2983e695ce728f6de91b800848
Tkappa <tkappa@live.it>
2018-12-03 23:48:08 +0100

    day3

--- day3.py
+++ day3.py
@@ -1,49 +1,57 @@
 with open('input3.txt') as f:
   lines= f.readlines()
   D = dict()
-  for line in lines:
-    spline = line.split("@")
-    values = spline[1].split(":")
-    coord = values[0].split(",")
 
-    coord[0] = coord[0].strip()
-    coord[1] = coord[1].strip()
-    amount = values[1].split("x")
-    amount[0]= amount[0].strip()
-    amount[1]= amount[1].strip()
+  # Part one
+  for line in lines:
+    # A input string il like this #1 @ 287,428: 27x20
+    # So coord is 287,428 and amount 27x20
+    coord = line.split("@")[1].split(":")[0].split(",")
+    amount = line.split("@")[1].split(":")[1].split("x")
+
+    coord= (int(coord[0].strip()), int(coord[1].strip() ))
+    amount= (int(amount[0].strip()),int(amount[1].strip()))
+
     indexx = 0
-    while(indexx<int(amount[0])):
+    while(indexx<amount[0]):
       indexy = 0
-      while(indexy<int(amount[1])):
-        newcord =[0,0]
-        newcord[0]= int(coord[0])+indexx
-        newcord[1]= int(coord[1])+indexy
-        scord = str(newcord[0]) + ','+ str(newcord[1])
-        if scord in D:
-          D[scord]+=1
+      while(indexy<amount[1]):
+        newcord = (coord[0]+indexx ,coord[1]+indexy)
+        if newcord in D:
+            D[newcord]+=1
         else:
-          D[scord]=1
+          D[newcord]=1
         indexy+=1
       indexx+=1
+
   total = 0
   for num in D:
     if(D[num]>1):
       total+=1
   print ("Part one: " + str(total))
-  
-  """
-  Part 2 work in progress
-  single = ""
-  for num in D:
-    if(D[num]==1):
-      single = num
-      print (num)
-      break
-  print (single)
+
+  #Part two
   for line in lines:
-    spline = line.split("@")
-    values = spline[1].split(":")
-    coord = values[0].strip()
-    if(coord==single):
-      print(spline[0]) 
-    """
+    coord = line.split("@")[1].split(":")[0].split(",")
+    amount = line.split("@")[1].split(":")[1].split("x")
+
+    coord= (int(coord[0].strip()), int(coord[1].strip() ))
+    amount= (int(amount[0].strip()),int(amount[1].strip()))
+
+    stillone=1 # Checks if the D[coord] value stays 1 while iterating
+    entered= 0 # Cheks if it has entered the cycle
+    if(D[coord]==1):
+        entered=1
+        indexx = 0
+        while(indexx<amount[0] and stillone==1):
+          indexy = 0
+          while(indexy<amount[1] and stillone==1):
+            newcord = (coord[0]+indexx ,coord[1]+indexy)
+            if(D[newcord]!=1):
+                stillone = 0
+            indexy+=1
+          indexx+=1
+    # If it has entered the cycle and has ended it without exiting early it means its the one without overlapping
+    if(stillone==1 and entered ==1):
+        print "Part two: "+ line.split("@")[0].replace('#','')
+        break
